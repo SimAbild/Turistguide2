24add6dde4a49ed3fd55c4368901322f2ab7d4d2
SimAbild <abildtrup123@gmail.com>
2025-09-03 09:13:05 +0200

    Lavet projektet

--- src/main/java/com/simon/turistguide2/Turistguide2Application.java
+++ src/main/java/com/simon/turistguide2/Turistguide2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Turistguide2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Turistguide2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Turistguide2Application.class, args);
+    }
 
 }
---
+++ src/main/java/com/simon/turistguide2/service/TouristService.java
@@ -0,0 +1,35 @@
+package com.simon.turistguide2.service;
+
+import com.simon.turistguide2.model.TouristAttraction;
+import com.simon.turistguide2.repository.TouristRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class TouristService {
+    private final TouristRespository repository;
+
+    public TouristService(TouristRespository repository) {
+        this.repository = repository;
+    }
+
+    public ArrayList<TouristAttraction> getTouristAttractions() {
+        return repository.getTouristAttractions();
+    }
+
+    public TouristAttraction findAttractionByName(String name){
+       return repository.findAttractionByName(name);
+    }
+
+    public TouristAttraction addAttraction(TouristAttraction touristAttraction) {
+        return repository.addAttraction(touristAttraction);
+    }
+    public ArrayList<TouristAttraction> deleteAttraction(String name){
+        return repository.deleteAttraction(name);
+    }
+
+    public ArrayList<TouristAttraction> updateAttraction(TouristAttraction touristAttraction) {
+        return repository.updateAttraction(touristAttraction);
+    }
+}
