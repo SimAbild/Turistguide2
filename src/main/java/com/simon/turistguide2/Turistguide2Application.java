41d7d8f5705ac65a175a046fcf2a7b68540a3e92
SimAbild <abildtrup123@gmail.com>
2025-09-03 09:13:05 +0200

    Lavet projektet

---
+++ src/main/java/com/simon/turistguide2/Turistguide2Application.java
@@ -0,0 +1,13 @@
+package com.simon.turistguide2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Turistguide2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Turistguide2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simon/turistguide2/Turistguide2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.turistguide2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turistguide2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
