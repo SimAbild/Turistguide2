e017756012b748ccdff2ce908b20b9239f189cb6
SimAbild <abildtrup123@gmail.com>
2025-10-01 10:53:13 +0200

    fjernet parameter for forsiden, så den er ikke attracion/attractions

--- src/main/java/com/simon/turistguide2/controller/TouristController.java
+++ src/main/java/com/simon/turistguide2/controller/TouristController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @Controller
-@RequestMapping("attraction")
+@RequestMapping()
 public class TouristController {
     private final TouristService touristService;
 
